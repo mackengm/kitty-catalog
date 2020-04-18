c09a176fb32678d5355afc46d4225ccdb3148fa9
Mackenzie  McFate <mackenziemcfate@Mackenzies-MacBook-Air.local>
2020-04-17 22:26:36 -0500

    fixing baseURL

--- push-update.sh
+++ push-update.sh
@@ -3,7 +3,7 @@ current=`git symbolic-ref --short -q HEAD`
 git checkout ${current}
 
 # Compile the site before copying to the new image.
-hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://catalog.m4ck3ngm.com
+hugo --ignoreCache --ignoreVendor --minify --debug --verbose --baseURL=https://kitty-catalog.m4ck3ngm.com
 echo "Kitty-Catalog Hugo compilation is complete."
 echo "Starting docker image build..."
 docker image build -f push-update-Dockerfile --no-cache -t kitty-catalog .
